09dd83248d25d55d248e184ec8e3aa0ca9172f76
Patrick-Davies <pdavies@ihsys.com>
2020-11-05 20:28:42 -0500

    Add some types to pointers in order to be c++ compliant

--- src/lv_core/lv_style.h
+++ src/lv_core/lv_style.h
@@ -311,7 +311,7 @@ void _lv_style_list_reset(lv_style_list_t * style_list);
 static inline lv_style_t * lv_style_list_get_style(lv_style_list_t * list, uint8_t id)
 {
     if(list->has_trans && list->skip_trans) id++;
-    if(list->style_cnt == 0 || id >= list->style_cnt) return NULL;
+    if(list->style_cnt == 0 || id >= list->style_cnt) return (lv_style_t *)NULL;
     return list->style_list[id];
 }
 
